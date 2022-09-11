c019666601b2c034de14c6e2d1611b4c36d6de01
Techbrolakes <lekandar11@gmail.com>
2022-09-11 22:40:26 +0100

    Full Config Setup

--- logger.config.ts
+++ logger.config.ts
@@ -1,12 +1,6 @@
-import constants from "@config/constants"
-
-const {development} = constants.ENVIRONMENT
-
 
 const logger = <T>(log : T): void => {
-    if (development) {
         console.log(log)
-     }
 }
 
 
